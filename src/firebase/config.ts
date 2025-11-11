e145a4f873d0aaad42e567ee039c251b9a665eae
Pedro Santorio <pedrohsantorio@gmail.com>
2025-11-11 21:51:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4585047835-36989",
+  "appId": "1:439208549505:web:11ca2034eb012cad120180",
+  "apiKey": "AIzaSyCKTW3-rGWuKxbaFFDd3IPYeu3XVH1YolU",
+  "authDomain": "studio-4585047835-36989.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "439208549505"
+};
