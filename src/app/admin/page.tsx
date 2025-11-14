9a8e1335c575a881b42eb4dc510b771fb6d62998
pedrohsantorio-maker <pedrohsantorio@gmail.com>
2025-11-13 23:07:22 -0300

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useUser } from '@/firebase';
-import { useRouter }s from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function AdminRootPage() {
